e61d236c14b7904d51aa96e19a2ed1308ceba6ae
jayeeed <jayedbinjahangir@gmail.com>
2024-03-12 03:10:42 +0600

    update

--- Home.py
+++ Home.py
@@ -1,28 +1,50 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Chatbot",
-    page_icon='💬',
-    layout='wide'
+    page_title="Cypher.ai",
+    page_icon='🤖',
+    layout='wide',
 )
 
-st.header("Chatbot Implementations with Langchain")
-st.write("""
-[![view source code](https://img.shields.io/badge/GitHub%20-gray?logo=github)](https://github.com/jayeeed/langchain-chatbot)
-![Visitors](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2Fjayeeed%2Flangchain-chatbot&label=Visitors&countColor=%23263759&style=flat)
+# st.write("""
+# [![view source code](https://img.shields.io/badge/GitHub%20-gray?logo=github)](https://github.com/jayeeed/langchain-chatbot)
+# ![Visitors](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2Fjayeeed%2Flangchain-chatbot&label=Visitors&countColor=%23263759&style=flat)
+# """)
+
+st.markdown("""
+# :blue[Cypher.ai] 🤖
+## Order your :rainbow[Personalized Chatbot] soon!
+&nbsp;
+#### 1. What is :blue[Cypher.ai]? 
+
+Cypher.ai offers a suite of **Chatbots** to help users interact with the data.
+
+#### 2. What are the :blue[Chatbots] available?
+
+- :green[**Q/ABot**]: Basic **Chatbot** that allows users to ask ***Questions*** and get ***Answers***.
+
+- :green[**ConvoBot**]: Conversational **Chatbot** that allows users ***Chat*** and ***Remember*** their previous interactions.
+         
+- :green[**NetBot**]: Internet accessed **Chatbot** that allows users to ask questions about ***Latest*** events.
+        
+- :green[**DocBot**]: **Chatbot** that allows users to ask questions about ***Documents***.
+         
+- :green[**HRBot**]: **Chatbot** that allows users to ask questions about all ***Documents*** in the ***Directories***.
+         
+- :green[**SumBot**]: **Chatbot** that allows users to ask questions about ***Summerization*** of ***Documents***.
+
+#### 3. How can I order :blue[Cypher.ai]?
+
+- **[Email](mailto:jayedbinjahangir@gmail.com) &nbsp;&nbsp; [Portfolio](https://jayeeed.netlify.app/) &nbsp;&nbsp; [Linkedin](https://www.linkedin.com/in/xayed/)**
+
+#### 4. How can I contact :blue[Cypher.ai]?
+
+- **[Email](mailto:jayedbinjahangir@gmail.com) &nbsp;&nbsp; [Portfolio](https://jayeeed.netlify.app/) &nbsp;&nbsp; [Linkedin](https://www.linkedin.com/in/xayed/)**
+
+#### 5. How can I get support for :blue[Cypher.ai]?
+
+- **[Email](mailto:jayedbinjahangir@gmail.com) &nbsp;&nbsp; [Portfolio](https://jayeeed.netlify.app/) &nbsp;&nbsp; [Linkedin](https://www.linkedin.com/in/xayed/)**
+         
 """)
 
-st.write("""
-Langchain is a powerful framework designed to streamline the development of applications using Language Models (LLMs). It provides a comprehensive integration of various components, simplifying the process of assembling them to create robust applications.
-
-Leveraging the power of Langchain, the creation of chatbots becomes effortless. Here are a few examples of chatbot implementations catering to different use cases:
-
-- **BasicBot**: Engage in interactive conversations with the LLM.
-- **ContextBot**: A chatbot that remembers previous conversations and provides responses accordingly.
-- **NetBot**: An internet-enabled chatbot capable of answering user queries about recent events.
-- **DocBot**: Empower the chatbot with the ability to access custom documents, enabling it to provide answers to user queries based on the referenced information.
-- **DirBot**: Empower the chatbot with the ability to access multiple documents, enabling it to provide answers to user queries based on the referenced information.
-- **SumBot**: Empower the chatbot with the ability to access multiple documents, enabling it to provide summary to user queries based on the referenced information.
-
-To explore sample usage of each chatbot, please navigate to the corresponding chatbot section.
-""")
+st.caption('Made with ❤️ by [Jayed](https://www.linkedin.com/in/xayed/)')
