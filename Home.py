f9bd62b4993523061b74251fc1d101384f7641ba
jayeeed <jayedbinjahangir@gmail.com>
2024-03-18 00:24:26 +0900

    auth

--- Home.py
+++ Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import hmac
 
 st.set_page_config(
     page_title="Cypher.ai",
@@ -11,6 +12,33 @@ st.set_page_config(
 # [![view source code](https://img.shields.io/badge/GitHub%20-gray?logo=github)](https://github.com/jayeeed/langchain-chatbot)
 # ![Visitors](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2Fjayeeed%2Flangchain-chatbot&label=Visitors&countColor=%23263759&style=flat)
 # """)
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 st.markdown("""
 # :blue[Cypher.ai] 🤖
